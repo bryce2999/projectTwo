ff6f979e6b0b006e6225a4a91ebb77fa23813535
Bryce Jackson <bryce.e.jackson-1@ou.edu>
2019-10-08 13:15:59 -0500

    Created class files and project structure

---
+++ Project 2/src/PosAvg.java
@@ -0,0 +1,15 @@
+
+public class PosAvg {
+
+	public PosAvg(String stID) 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public String indexOfStation() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
